e44ed42dd115bbb5319cf2b54d2503ab38870b4d
Sebastián Flores <sebflodev@gmail.com>
2023-11-12 22:36:33 -0300

    part1.11-p1

--- part6/src/App.jsx
+++ part6/src/App.jsx
@@ -10,27 +10,42 @@ const Button = ({ onClick, text }) => {
 
 const Statistics = ({good, neutral, bad}) => {
     const all = good + neutral + bad
+    const average = (good - bad) / all
+    const positive = `${(good / all) * 100} %`
 
     const StatisticLine = ({texto, value}) => {
         return (
-            <>
-                <p>{texto} {value}</p>
-            </>
+            <tr>
+                <td>{texto}</td> 
+                <td>{value}</td>
+            </tr>
         )
     }
 
     return all > 0 ? (
         <>
             <h1>statistics</h1>
-            <StatisticLine texto="good" value={good} />
-            <StatisticLine texto="neutral" value={neutral} />
-            <StatisticLine texto="bad" value={bad} />
-            <p>all {all}</p>
-            <p>average {(good - bad) / all}</p>
-            <p>positive {good / all * 100} %</p>
+            <table>
+                <tbody>
+                    <StatisticLine texto="good" value={good} />
+                    <StatisticLine texto="neutral" value={neutral} />
+                    <StatisticLine texto="bad" value={bad} />
+                    <StatisticLine texto="all" value={all} />
+                    <StatisticLine texto="average" value={average} />
+                    <StatisticLine texto="positive" value={positive} />
+                </tbody>
+            </table>
         </>
     ) : (
         <p>No feedback given</p>
+    )
+}
+
+const Anecdotes = (props) => {
+    return (
+        <div>
+            {props.anecdotes}
+        </div>
     )
 }
 
@@ -39,8 +54,18 @@ export const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [selected, setSelected] = useState(0)
 
     const increment = (status, setStatus) => () => setStatus(status + 1) 
+
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+      ]
     
     return (
         <div>
@@ -49,6 +74,8 @@ export const App = () => {
             <Button onClick={increment(neutral, setNeutral)} text={'neutral'} />
             <Button onClick={increment(bad, setBad)} text={'bad'} />
             <Statistics good={good} neutral={neutral} bad={bad} />
+            <Button onClick={increment(selected, setSelected)} text={'anectodes'}/>
+            <Anecdotes anecdotes={anecdotes[selected]} />
         </div>
     )
 }
